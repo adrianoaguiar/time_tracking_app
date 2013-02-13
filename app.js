47a143620f0252fe7b7df5f08d000c713075744b
Pierre Nespo <pierre.nespo@gmail.com>
2013-02-13 14:57:57 +0100

    Allow the user to enter a custom time instead of the autofilled one

--- app.js
+++ app.js
@@ -1,195 +1,210 @@
 (function(){
-    return {
-        appID:  'simple time tracking',
-        defaultState: 'loading',
-        startedTime: '',
-        baseHistory: '',
-        baseTime: '',
-        counterStarted: false,
-        events: {
-            'app.activated': 'onActivated',
-            'ticket.requester.email.changed': 'loadIfDataReady',
-            'click #time-tracker-submit': 'submit'
-        },
+  return {
+    appID:  'simple time tracking',
+    defaultState: 'loading',
+    startedTime: '',
+    baseHistory: '',
+    baseTime: '',
+    counterStarted: false,
 
-        onActivated: function(data){
-            this.doneLoading = false;
-            this.loadIfDataReady();
-        },
+    events: {
+      'app.activated'                           : 'onActivated',
+      'ticket.requester.email.changed'          : 'loadIfDataReady',
+      'click #time-tracker-submit'              : 'submit',
+      'click #time-tracker-custom-submit'       : 'submitCustom'
+    },
 
-        loadIfDataReady: function(){
-            var ticket = this.ticket(),
-            requester = ticket.requester(),
-            requesterEmail = requester && requester.email();
+    onActivated: function(data){
+      this.doneLoading = false;
+      this.loadIfDataReady();
+    },
 
-            if (!this.doneLoading && !_.isEmpty(requesterEmail)) {
-                if (this.ticket().status() === 'new')
-                    return;
+    loadIfDataReady: function(){
+      var ticket = this.ticket(),
+      requester = ticket.requester(),
+      requesterEmail = requester && requester.email();
 
-                if (!this.counterStarted){
-                    this.startCounter();
-                    this.counterStarted = true;
-                }
+      if (!this.doneLoading && !_.isEmpty(requesterEmail)) {
+        if (this.ticket().status() === 'new')
+          return;
 
-                services.appsTray().show();
-
-                this.baseHistory = this._historyField() || '';
-                this.baseTime = this._timeField();
-
-                this._timeFieldUI().disable();
-                this._historyFieldUI().disable();
-
-                this.switchTo('form');
-
-                this.setDefaults();
-                this.doneLoading = true;
-            }
-        },
-
-
-        startCounter: function(){
-            if (_.isEmpty(this.timeLoopID)){
-                this.startedTime = new Date();
-                this.timeLoopID = this.setTimeLoop(this);
-            }
-        },
-
-        submit: function(event){
-            event.preventDefault();
-            this.addTime();
-            this.enableSave();
-            this.disableSaveOnTimeout(this);
-        },
-
-        disableSaveOnTimeout: function(self){
-            return setTimeout(function(){
-                self.disableSave();
-            }, self._disableSaveInterval());
-        },
-
-        setTimeLoop: function(self){
-            return setInterval(function(){
-                if (_.isEmpty(self.$('#time'))){
-                    clearInterval(self.timeLoopID);
-                } else {
-                    var ms = self.setWorkedTime();
-
-                    if (ms > self._thresholdToStart() &&
-                        _.isUndefined(self.thresholdReached)){
-                        self.disableSave();
-                        self.$('#submit-container').show();
-                        self.thresholdReached = true;
-                    }
-                }
-            }, 1000);
-        },
-
-        // Returns worded time as ms
-        setWorkedTime: function(){
-            var ms = this._elapsedTime();
-            var elapsedTime = this._msToHuman(ms);
-
-            this.$('#time').html(this._prettyTime(elapsedTime));
-
-            return ms;
-        },
-
-        setDefaults: function(){
-            if (_.isEmpty(this.$('#date').text()))
-                this.$("#date").html(this._formattedDate());
-
-            this.setWorkedTime();
-
-            if (_.isUndefined(this.thresholdReached)){
-                this.$('#submit-container').hide();
-            } else{
-                this.$('#submit-container').show();
-            }
-        },
-
-        addTime: function() {
-            var newTime = this._prettyTime(this.calculateNewTime());
-            var newHistory = this.baseHistory + '\n' +  this.currentUser().name() +
-                ',' + newTime + ',' + this._formattedDate('yyyy-mm-dd') + '';
-
-            this.ticket().customField(this._timeFieldLabel(), newTime);
-            this.ticket().customField(this._historyFieldLabel(), newHistory);
-
-            this.enableSave();
-        },
-        calculateNewTime: function(){
-            var oldTime = (this.baseTime || '00:00:00').split(':'),
-                currentElapsedTime = this._elapsedTime(),
-                hours = parseInt((oldTime[0] || 0), 0),
-                minutes = parseInt((oldTime[1] || 0), 0),
-                seconds = parseInt((oldTime[2] || 0), 0);
-
-            var newTime = (parseInt(hours, 0) * 3600000) +
-                (parseInt(minutes, 0) * 60000) +
-                (parseInt(seconds, 0) * 1000) +
-                currentElapsedTime;
-
-            return this._msToHuman(newTime);
-        },
-        _thresholdToStart: function(){
-            return ((parseInt(this.settings.start_threshold, 0) || 15) * 1000);
-        },
-        _disableSaveInterval: function(){
-            return ((parseInt(this.settings.block_save_interval, 0) || 5) * 1000);
-        },
-        _formattedDate: function(format){
-            var dateString = format || this._dateFormat(),
-                date = new Date();
-
-            dateString = dateString.replace('dd', this._addSignificantZero(date.getDate()));
-            dateString = dateString.replace('mm', this._addSignificantZero(date.getMonth() + 1));
-
-            return dateString.replace('yyyy', date.getFullYear());
-        },
-        _msToHuman: function(ms){
-            var time = parseInt((ms / 1000), 0);
-            var seconds = time % 60;
-            time = parseInt(time/60, 0);
-            var minutes = time % 60;
-            var hours = parseInt(time/60, 0) % 24;
-
-            return {
-                seconds: seconds,
-                minutes: minutes,
-                hours: hours
-            };
-        },
-        _addSignificantZero: function(num){
-            return((num < 10 ? '0' : '') + num);
-        },
-        _elapsedTime: function(){
-            return new Date() - this.startedTime;
-        },
-        _prettyTime: function(time){
-            return this._addSignificantZero(time.hours) + ':' +
-                this._addSignificantZero(time.minutes) + ':' +
-                this._addSignificantZero(time.seconds);
-        },
-        _timeFieldUI: function(){
-            return this.ticketFields(this._timeFieldLabel());
-        },
-        _historyFieldUI: function(){
-            return this.ticketFields(this._historyFieldLabel());
-        },
-        _timeField: function(){
-            return this.ticket().customField(this._timeFieldLabel());
-        },
-        _historyField: function(){
-            return this.ticket().customField(this._historyFieldLabel());
-        },
-        _timeFieldLabel: function(){
-            return 'custom_field_' + this.settings.totaltime;
-        },
-        _historyFieldLabel: function(){
-            return 'custom_field_' + this.settings.timehistory;
-        },
-        _dateFormat: function(){
-            return this.settings.date_format || 'yyyy-mm-dd';
+        if (!this.counterStarted){
+          this.startCounter();
+          this.counterStarted = true;
         }
-    };
+
+        services.appsTray().show();
+
+        this.baseHistory = this._historyField() || '';
+        this.baseTime = this._timeField();
+
+        this._timeFieldUI().disable();
+        this._historyFieldUI().disable();
+
+        this.switchTo('form');
+
+        this.setDefaults();
+        this.doneLoading = true;
+      }
+    },
+
+    startCounter: function(){
+      if (_.isEmpty(this.timeLoopID)){
+        this.startedTime = new Date();
+        this.timeLoopID = this.setTimeLoop(this);
+      }
+    },
+
+    submit: function(event){
+      event.preventDefault();
+      this.addTime();
+      this.disableSaveOnTimeout(this);
+    },
+
+    submitCustom: function(){
+      this.addTime(this._humanToMs(this.$('#custom_time').val()));
+      this.disableSaveOnTimeout(this);
+      this.$('#customTimeModal').modal('hide');
+    },
+
+    disableSaveOnTimeout: function(self){
+      return setTimeout(function(){
+        self.disableSave();
+      }, self._disableSaveInterval());
+    },
+
+    setTimeLoop: function(self){
+      return setInterval(function(){
+        if (_.isEmpty(self.$('#time'))){
+          clearInterval(self.timeLoopID);
+        } else {
+          var ms = self.setWorkedTime();
+
+          if (ms > self._thresholdToStart() &&
+              _.isUndefined(self.thresholdReached)){
+            self.disableSave();
+            self.$('#submit-container').show();
+            self.thresholdReached = true;
+          }
+        }
+      }, 1000);
+    },
+
+    // Returns worded time as ms
+    setWorkedTime: function(){
+      var ms = this._elapsedTime();
+      var elapsedTime = this._msToHuman(ms);
+
+      this.$('#time').html(this._prettyTime(elapsedTime));
+
+      return ms;
+    },
+
+    setDefaults: function(){
+      if (_.isEmpty(this.$('#date').text()))
+        this.$("#date").html(this._formattedDate());
+
+      this.setWorkedTime();
+
+      if (_.isUndefined(this.thresholdReached)){
+        this.$('#submit-container').hide();
+      } else{
+        this.$('#submit-container').show();
+      }
+    },
+
+    addTime: function(time) {
+      var newTime = (_.isUndefined(time) ? this.calculateNewTime() :
+                     this.calculateNewTime(time));
+
+      newTime = this._prettyTime(newTime);
+
+      var newHistory = this.baseHistory + '\n' +  this.currentUser().name() +
+        ',' + newTime + ',' + this._formattedDate('yyyy-mm-dd') + '';
+
+      this.ticket().customField(this._timeFieldLabel(), newTime);
+      this.ticket().customField(this._historyFieldLabel(), newHistory);
+
+      this.enableSave();
+    },
+    calculateNewTime: function(time){
+      var oldTime = (this.baseTime || '00:00:00').split(':'),
+      currentElapsedTime = (_.isUndefined(time) ? this._elapsedTime() : time),
+      hours = parseInt((oldTime[0] || 0), 0),
+      minutes = parseInt((oldTime[1] || 0), 0),
+      seconds = parseInt((oldTime[2] || 0), 0);
+
+      var newTime = (parseInt(hours, 0) * 3600000) +
+        (parseInt(minutes, 0) * 60000) +
+        (parseInt(seconds, 0) * 1000) +
+        currentElapsedTime;
+
+      return this._msToHuman(newTime);
+    },
+    _thresholdToStart: function(){
+      return ((parseInt(this.settings.start_threshold, 0) || 15) * 1000);
+    },
+    _disableSaveInterval: function(){
+      return ((parseInt(this.settings.block_save_interval, 0) || 5) * 1000);
+    },
+    _formattedDate: function(format){
+      var dateString = format || this._dateFormat(),
+      date = new Date();
+
+      dateString = dateString.replace('dd', this._addSignificantZero(date.getDate()));
+      dateString = dateString.replace('mm', this._addSignificantZero(date.getMonth() + 1));
+
+      return dateString.replace('yyyy', date.getFullYear());
+    },
+    _msToHuman: function(ms){
+      var time = parseInt((ms / 1000), 0);
+      var seconds = time % 60;
+      time = parseInt(time/60, 0);
+      var minutes = time % 60;
+      var hours = parseInt(time/60, 0) % 24;
+
+      return {
+        seconds: seconds,
+        minutes: minutes,
+        hours: hours
+      };
+    },
+    _humanToMs: function(timestring){
+      var time = timestring.split(':').map(function(i){return parseInt(i, 0);});
+
+      return ((time[0] * 3600) + (time[1] * 60) + time[2])*1000;
+    },
+    _addSignificantZero: function(num){
+      return((num < 10 ? '0' : '') + num);
+    },
+    _elapsedTime: function(){
+      return new Date() - this.startedTime;
+    },
+    _prettyTime: function(time){
+      return this._addSignificantZero(time.hours) + ':' +
+        this._addSignificantZero(time.minutes) + ':' +
+        this._addSignificantZero(time.seconds);
+    },
+    _timeFieldUI: function(){
+      return this.ticketFields(this._timeFieldLabel());
+    },
+    _historyFieldUI: function(){
+      return this.ticketFields(this._historyFieldLabel());
+    },
+    _timeField: function(){
+      return this.ticket().customField(this._timeFieldLabel());
+    },
+    _historyField: function(){
+      return this.ticket().customField(this._historyFieldLabel());
+    },
+    _timeFieldLabel: function(){
+      return 'custom_field_' + this.settings.totaltime;
+    },
+    _historyFieldLabel: function(){
+      return 'custom_field_' + this.settings.timehistory;
+    },
+    _dateFormat: function(){
+      return this.settings.date_format || 'yyyy-mm-dd';
+    }
+  };
 }());
